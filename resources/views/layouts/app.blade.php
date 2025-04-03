ba4c20fc120c5f3caefd7056d9e17c08ccd8bbf8
Pasha Ahmad <pasha.pasha12e@gmail.com>
2025-04-03 16:12:01 +0200

    wip

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,10 +2,12 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>@yield('title', 'Your App')</title>
-    @vite('resources/css/app.css')
+    <title>@yield('title', 'De Bazaar')</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script src="https://kit.fontawesome.com/bb073b0d89.js" crossorigin="anonymous"></script>
 </head>
-<body class="bg-gray-50">
-@yield('content')
+<body class="font-sans antialiased text-gray-900 bg-gray-100">
+@yield('body')
 </body>
 </html>
