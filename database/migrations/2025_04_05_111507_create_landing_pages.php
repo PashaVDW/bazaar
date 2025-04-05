126c3bff9ff8c90b8bc31418177fa69ba3c143e5
Darlon <darlonmpeeters@gmail.com>
2025-04-05 13:21:02 +0200

    wip

---
+++ database/migrations/2025_04_05_111507_create_landing_pages.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('landing_pages', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('component_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('landing_page_id')->constrained()->cascadeOnDelete();
+            $table->integer('order')->default(0); 
+            $table->json('settings')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('landing_pages');
+    }
+};
